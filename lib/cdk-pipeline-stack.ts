4bab47771e353e2eacd708a756a71e910030ded2
vfSleeth <michael.sleeth@ttec.com>
2022-03-13 20:07:55 -0500

    fixes logical name for backend stack clash

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -21,10 +21,14 @@ export class MyPipelineStack extends cdk.Stack {
 
     const wave = pipeline.addWave('wave');
     wave.addStage(
-      new DeployLambdasAppStage(this, 'lambda-backend-stage', { env: { account: '273049437864', region: 'us-west-2' } })
+      new DeployLambdasAppStage(this, 'lambda-backend-stage-t2', {
+        env: { account: '273049437864', region: 'us-west-2' },
+      })
     );
     wave.addStage(
-      new DeployLambdasAppStage(this, 'lambda-backend-stage', { env: { account: '500573939214', region: 'us-east-1' } })
+      new DeployLambdasAppStage(this, 'lambda-backend-stage-personal', {
+        env: { account: '500573939214', region: 'us-east-1' },
+      })
     );
 
     wave.addPost(new ManualApprovalStep('approval'));
